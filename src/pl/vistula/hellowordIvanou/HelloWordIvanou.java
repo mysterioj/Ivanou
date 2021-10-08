0863c16b3f92a5ba8cd8d08c604c7764ea048ec2
student <student@lab.pl.wsei>
2021-10-08 13:59:00 +0200

    Initial commit

---
+++ src/pl/vistula/hellowordIvanou/HelloWordIvanou.java
@@ -0,0 +1,9 @@
+package pl.vistula.hellowordIvanou;
+
+public class HelloWordIvanou {
+    public static void main(String[] args) {
+        System.out.println(HelloVistulaIvanou);
+        
+    }
+
+}
